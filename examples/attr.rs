395ced4d13c28066a24019aedf9baa7b59a8f470
Shirayama Kazatsuyu <shirayama.kazatsuyu@gmail.com>
2022-09-18 11:03:00 +0900

    Update examples.

--- examples/attr.rs
+++ examples/attr.rs
@@ -1,11 +1,19 @@
 #![feature(stmt_expr_attributes, proc_macro_hygiene)]
 use str_match::str_match;
 
-fn main() {
-    let a = "abcdefgh";
+fn f(a: &str) -> &str {
     #[str_match]
     match a {
-        "abc{x}fgh" => println!("{x}"),
-        _ => {}
+        "abc{x}ghi" => x,
+        "aaa{x}" => x,
+        "{{{x}}}" => x,
+        _ => "!",
     }
+}
+
+fn main() {
+    assert_eq!(f("abcdefghi"), "def");
+    assert_eq!(f("aaabbbccc"), "bbbccc");
+    assert_eq!(f("{000}"), "000");
+    assert_eq!(f("xyz"), "!");
 }
